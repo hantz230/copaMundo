4b602e654e6dff50a2ebc1b4387400d6667e87ed
hantz230 <dpf.cristian@gmail.com>
2026-06-08 08:36:47 -0300

    Initial commit

---
+++ src/main/java/Exemplo/copaMundo/CopaMundoApplication.java
@@ -0,0 +1,13 @@
+package Exemplo.copaMundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopaMundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopaMundoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Exemplo/copaMundo/CopaMundoApplicationTests.java
@@ -0,0 +1,13 @@
+package Exemplo.copaMundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopaMundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
